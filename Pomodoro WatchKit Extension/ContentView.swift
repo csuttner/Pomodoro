f1021329fd83ce7f7cf0ae68cd824afd6cc7ad2c
Clay Suttner <csuttner@gmail.com>
2021-04-28 22:15:07 -0700

    Migrates project to SwiftUI

---
+++ Pomodoro WatchKit Extension/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Pomodoro WatchKit Extension
+//
+//  Created by Clay Suttner on 4/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pomodoro WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  Pomodoro WatchKit Extension
+//
+//  Created by Clay Suttner on 4/28/21.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
